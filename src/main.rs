d839868f61d31215ae71dbe0106e068abe925c4e
Mateus Santos <mateus.freira@gmail.com>
2018-12-07 18:35:26 -0200

    working with only one client

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ extern crate futures;
 use std::net::{TcpListener, TcpStream, SocketAddr};
 use std::io::Result;
 use std::io::{BufReader, BufRead, BufWriter, Write};
+
+use futures::Async;
+use futures::Future;
+
+
+use std::sync::mpsc::channel;
+use std::sync::mpsc::{Sender, Receiver};
+
 use std::thread;
 use std::sync::{Arc, Mutex};
 use std::collections::HashMap;
@@ -12,7 +20,7 @@ struct Database {
 }
 
 struct Watchers {
-	map: Mutex<HashMap<SocketAddr, futures::sync::mpsc::UnboundedSender<String>>>,
+	map: Mutex<HashMap<String, Sender<String>>>,
 }
 
 
@@ -20,83 +28,92 @@ struct Watchers {
 fn handle_client(stream: TcpStream, db: Arc<Database>, watchers:Arc<Watchers>) {
 	let mut reader = BufReader::new(&stream);
 	let mut writer = BufWriter::new(&stream);
-	loop {
-		let mut buf = String::new();
-		let read_line = reader.read_line(&mut buf);
-		match read_line {
-			Ok(_) => {
-				let mut db = db.map.lock().unwrap();
-                let mut command  = buf.splitn(3, " ");
-					match command.next() {
-						Some("watch") => {
-							let key = match command.next() {
-								Some(key) => key.replace("\n", ""),
-								None => {
-									println!("WATCH must be followed by a key");
-									"Error".to_string()
-								}
-							};
-							let value = match db.get(&key.to_string()) {
-                            	Some(value) => value,
-                            	None => "<Empty>",
-                        	};
-							writer.write_fmt(format_args!("{}", value)).unwrap();
-							writer.flush();
-							println!("<WATCH> {}  {}", key, value)
-						}
-						Some("get") => {
-							let key = match command.next() {
-								Some(key) => key.replace("\n", ""),
-								None => {
-									println!("GET must be followed by a key");
-									"Error".to_string()
-								}
-							};
-							if command.next().is_some() {
-								//return Err(format!("GET's key must not be followed by anything"))
-								println!("GET's key must not be followed by anything")
-							}
-							let value = match db.get(&key.to_string()) {
-                            	Some(value) => value,
-                            	None => "<Empty>",
-                        	};
-							writer.write_fmt(format_args!("{}", value)).unwrap();
-							writer.flush();
-							println!("<GET> {}  {}", key, value)
-						}
-						Some("set") => {
-							let key = match command.next() {
-								Some(key) => key,
-								None => {
-									println!("SET must be followed by a key");
-									""
-								},
-							};
-							let value = match command.next() {
-								Some(value) => value.replace("\n", ""),
-								None => {
-									println!("SET needs a value");
-									"".to_string()
-								},
-							};
-							db.insert(key.clone().to_string(), value.clone().to_string());
-							println!("<SET> {}  {}", key, value);
-							let mut watchers = watchers.map.lock().unwrap();
-							let iter = watchers.iter_mut()
-											.map(|(_, v)| v);
-							for tx in iter {
-								tx.unbounded_send(format!("Broadcast: key: {} value: value: {}", key, value)).unwrap();
-							}
-							()
-						}
-						Some(cmd) => println!("unknown command: {}", cmd),
-						None => println!("empty input"),
-				}
-				println!("Command print: {}", buf);
-            },
-			_ => {}
-		}
-	}
+    let (sender, receiver): (Sender<String>, Receiver<String>) = channel();
+
+	loop {  
+            let mut buf = String::new();
+            let read_line = reader.read_line(&mut buf);
+            stream.set_nonblocking(true).unwrap();
+            match read_line {
+                Ok(_) => {
+                    let mut db = db.map.lock().unwrap();
+                    let mut command  = buf.splitn(3, " ");
+                        match command.next() {
+                            Some("watch") => {
+                                let key = match command.next() {
+                                    Some(key) => key.replace("\n", ""),
+                                    None => {
+                                        println!("WATCH must be followed by a key");
+                                        "Error".to_string()
+                                    }
+                                };
+                                let value = match db.get(&key.to_string()) {
+                                    Some(value) => value,
+                                    None => "<Empty>",
+                                };
+
+                                watchers.map.lock().unwrap().insert(key.clone(), sender.clone());
+                                println!("<WATCH> {}  {}", key, value)
+                            }
+                            Some("get") => {
+                                let key = match command.next() {
+                                    Some(key) => key.replace("\n", ""),
+                                    None => {
+                                        println!("GET must be followed by a key");
+                                        "Error".to_string()
+                                    }
+                                };
+                                if command.next().is_some() {
+                                    //return Err(format!("GET's key must not be followed by anything"))
+                                    println!("GET's key must not be followed by anything")
+                                }
+                                let value = match db.get(&key.to_string()) {
+                                    Some(value) => value,
+                                    None => "<Empty>",
+                                };
+                                sender.send(value.to_string());
+                                println!("<GET> {}  {}", key, value)
+                            }
+                            Some("set") => {
+                                let key = match command.next() {
+                                    Some(key) => key,
+                                    None => {
+                                        println!("SET must be followed by a key");
+                                        ""
+                                    },
+                                };
+                                let value = match command.next() {
+                                    Some(value) => value.replace("\n", ""),
+                                    None => {
+                                        println!("SET needs a value");
+                                        "".to_string()
+                                    },
+                                };
+                                db.insert(key.clone().to_string(), value.clone().to_string());
+                                match watchers.map.lock().unwrap().get(key.clone()) {
+                                Some(sender) => {
+                                    sender.send(value.clone());
+                                }
+                                 _ => {}
+                                }
+                                println!("<SET> {}  {}", key, value);
+                            }
+                            Some(cmd) => println!("unknown command: {}", cmd),
+                            None => println!("empty input"),
+                    }
+                    println!("Command print: {}", buf);
+                },
+                _ => {
+                    match receiver.try_recv() {
+                        Ok(message) => {
+                            writer.write_fmt(format_args!("{}", message)).unwrap();
+                            writer.flush();
+                        },
+                        _ => ()
+                    }
+                }
+            }
+    }
 }
 
 fn main() -> Result<()>{
@@ -119,15 +136,6 @@ fn main() -> Result<()>{
         thread::spawn(move || {
             match stream {
                 Ok(socket) => {
-                    let addr = socket.peer_addr().unwrap();
-                    let (tx, rx) = futures::sync::mpsc::unbounded();
-                    //Use channel 
-					/*let socket_writer = rx.fold(writer, |writer, msg| {
-						let amt = io::write_all(writer, msg.into_bytes());
-						let amt = amt.map(|(writer, _)| writer);
-						amt.map_err(|_| ())
-					});*/
-                    watchers.map.lock().unwrap().insert(addr, tx);
                     handle_client(socket, db, watchers);
                 },
                 _ => ()
