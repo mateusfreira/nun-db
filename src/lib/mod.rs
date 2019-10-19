dad4eeeb0488c471b2a2b0c9028bf20f9690e2ea
Mateus Santos <mateus.freira@gmail.com>
2019-10-19 17:20:29 -0300

    Implement auth command

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,164 +1,185 @@
-use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc::{Receiver, Sender};
 use std::sync::{Arc, Mutex};
 
 use std::collections::HashMap;
 
 pub struct Database {
-	pub map: Mutex<HashMap<String, String>>,
+    pub map: Mutex<HashMap<String, String>>,
 }
 
 pub struct Watchers {
-	pub map: Mutex<HashMap<String, Vec<Sender<String>>>>,
+    pub map: Mutex<HashMap<String, Vec<Sender<String>>>>,
 }
 
 pub enum Request {
     Get { key: String },
     Set { key: String, value: String },
     Watch { key: String },
+    Auth { user: String, password: String },
 }
 
 pub enum Response {
     Value { key: String, value: String },
-    Ok {} ,
-    Set { key: String, value: String},
+    Ok {},
+    Set { key: String, value: String },
     Error { msg: String },
 }
 
-
 pub struct SenderBox {
-	pub sender: Mutex<Sender<String>>,
+    pub sender: Mutex<Sender<String>>,
 }
 
 impl Request {
-     pub fn parse(input: &str) -> Result<Request, String> {
-        let mut command  = input.splitn(3, " ");
-        let parsed_command  = match command.next() {
+    pub fn parse(input: &str) -> Result<Request, String> {
+        let mut command = input.splitn(3, " ");
+        let parsed_command = match command.next() {
             Some("watch") => {
                 let key = match command.next() {
                     Some(key) => key.replace("\n", ""),
-                    None => {
-                        return Err(format!("watch must contain a key")) 
-                    }
+                    None => return Err(format!("watch must contain a key")),
                 };
-                Ok(Request::Watch{ key })
-            },
+                Ok(Request::Watch { key })
+            }
             Some("get") => {
                 let key = match command.next() {
                     Some(key) => key.replace("\n", ""),
-                    None => {
-                        return Err(format!("get must contain a key")) 
-                    }
+                    None => return Err(format!("get must contain a key")),
                 };
-                Ok(Request::Get{ key })
-            },
-            Some("set") =>  {
+                Ok(Request::Get { key })
+            }
+            Some("set") => {
                 let key = match command.next() {
                     Some(key) => key,
                     None => {
                         println!("SET must be followed by a key");
                         ""
-                    },
+                    }
                 };
                 let value = match command.next() {
                     Some(value) => value.replace("\n", ""),
                     None => {
                         println!("SET needs a value");
                         "".to_string()
-                    },
+                    }
                 };
-                Ok(Request::Set { key: key.to_string(), value: value.to_string() })
-            },
+                Ok(Request::Set {
+                    key: key.to_string(),
+                    value: value.to_string(),
+                })
+            }
+            Some("auth") => {
+                let user = match command.next() {
+                    Some(key) => key,
+                    None => {
+                        println!("Auth needs to provide an user");
+                        ""
+                    }
+                };
+                let pwd = match command.next() {
+                    Some(pwd) => pwd.to_string(),
+                    None => {
+                        println!("Auth needs and password");
+                        "".to_string()
+                    }
+                };
+                Ok(Request::Auth {
+                    user: user.to_string(),
+                    password: pwd.to_string(),
+                })
+            }
             Some(cmd) => Err(format!("unknown command: {}", cmd)),
             _ => Err(format!("no command sent")),
         };
         parsed_command
-     }
+    }
 }
-
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn should_parse_auth() -> Result<(), String> {
+        match Request::parse("auth foo bar") {
+            Ok(Request::Auth { user, password }) => {
+                if (user == "foo" && password == "bar") {
+                    Ok(())
+                } else {
+                    Err(String::from("user should be foo and password bar"))
+                }
+            }
+            _ => Err(String::from("get foo sould be parsed to Get command")),
+        }
+    }
     #[test]
     fn should_parse_get() -> Result<(), String> {
-		match Request::parse("get foo") {
-            Ok(Request::Get{ key }) => {
-                if(key == "foo") {
-			        Ok(())
+        match Request::parse("get foo") {
+            Ok(Request::Get { key }) => {
+                if (key == "foo") {
+                    Ok(())
                 } else {
                     Err(String::from("the key should be foo"))
                 }
-            },
-            _ => {
-                Err(String::from("get foo sould be parsed to Get command"))
             }
-        } 
+            _ => Err(String::from("get foo sould be parsed to Get command")),
+        }
     }
 
     #[test]
     fn should_parse_get_ending_with_end_line() -> Result<(), String> {
-		match Request::parse("get foo\n") {
-            Ok(Request::Get{ key }) => {
-                if(key == "foo") {
-			        Ok(())
+        match Request::parse("get foo\n") {
+            Ok(Request::Get { key }) => {
+                if (key == "foo") {
+                    Ok(())
                 } else {
                     Err(String::from("the key should be foo"))
                 }
-            },
-            _ => {
-                Err(String::from("get foo sould be parsed to Get command"))
             }
-        } 
+            _ => Err(String::from("get foo sould be parsed to Get command")),
+        }
     }
-
-
 
     #[test]
     fn should_parse_set_ending_with_end_line() -> Result<(), String> {
-		match Request::parse("set foo 1\n") {
-            Ok(Request::Set{ key, value}) => {
-                if(key == "foo" && value == "1") {
-			        Ok(())
+        match Request::parse("set foo 1\n") {
+            Ok(Request::Set { key, value }) => {
+                if (key == "foo" && value == "1") {
+                    Ok(())
                 } else {
-                    Err(String::from("the key should be foo and the value should be 1"))
+                    Err(String::from(
+                        "the key should be foo and the value should be 1",
+                    ))
                 }
-            },
-            _ => {
-                Err(String::from("get foo sould be parsed to Set command"))
             }
-        } 
+            _ => Err(String::from("get foo sould be parsed to Set command")),
+        }
     }
- 
+
     #[test]
     fn should_parse_watch_ending_with_end_line() -> Result<(), String> {
-		match Request::parse("watch foo\n") {
-            Ok(Request::Watch{ key }) => {
-                if(key == "foo") {
-			        Ok(())
+        match Request::parse("watch foo\n") {
+            Ok(Request::Watch { key }) => {
+                if (key == "foo") {
+                    Ok(())
                 } else {
                     Err(String::from("the key should be foo"))
                 }
-            },
-            _ => {
-                Err(String::from("get foo sould be parsed to watch command"))
             }
-        } 
+            _ => Err(String::from("get foo sould be parsed to watch command")),
+        }
     }
 
     #[test]
     fn should_return_error_if_no_command_is_send() -> Result<(), String> {
-		match Request::parse("a\n") {
+        match Request::parse("a\n") {
             Err(msg) => {
-                if( msg == "unknown command: a\n") {
+                if (msg == "unknown command: a\n") {
                     Ok(())
                 } else {
                     Err(String::from("message it wrong"))
                 }
-            },
-            _ => {
-                Err(String::from("get foo sould be parsed to watch command"))
             }
-        } 
+            _ => Err(String::from("get foo sould be parsed to watch command")),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,185 +1,243 @@
+extern crate env_logger;
 extern crate futures;
 extern crate ws;
-extern crate env_logger;
-
 
 mod lib;
 
+use std::env;
 use ws::{listen, CloseCode, Handler, Message};
 
-use std::io::{BufReader, BufRead, BufWriter, Write};
+use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::net::{TcpListener, TcpStream};
 
 use std::thread;
 use std::time;
 
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::mpsc::channel;
-use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc::{Receiver, Sender};
 use std::sync::{Arc, Mutex};
 
 use std::collections::HashMap;
 
-use lib::{*};
-const TO_CLOSE:&'static str = "##CLOSE##";
+use lib::*;
+const TO_CLOSE: &'static str = "##CLOSE##";
 
-fn process_request (input: &str, watchers:Arc<Watchers>, sender:Sender<String>, db:Arc<Database>) -> Response {
-    let request = match Request::parse(input){
+fn process_request(
+    input: &str,
+    watchers: Arc<Watchers>,
+    sender: Sender<String>,
+    db: Arc<Database>,
+    auth: Arc<AtomicBool>,
+) -> Response {
+    let request = match Request::parse(input) {
         Ok(req) => req,
         Err(e) => return Response::Error { msg: e },
     };
-	match request {
-        Request::Watch { key  } => {
-            let mut watchers = watchers.map.lock().unwrap();
-            let mut senders:Vec<Sender<String>> = match watchers.get(&key) {
-                Some(mut watchers_vec) =>  {
-                    watchers_vec.clone()
-                }, 
-                _ => Vec::new()
+    match request {
+        Request::Auth { user, password } => {
+            let valid_user = match env::args().nth(1) {
+                Some(user) => user.to_string(),
+                _ => "".to_string()
             };
-            senders.push(sender.clone());
-            watchers.insert(key.clone(), senders.clone());
-            Response::Ok{}
-        },
+
+            let valid_pwd = match env::args().nth(2) {
+                Some(pwd) => pwd.to_string(),
+                _ => "".to_string()
+            };
+            if user == valid_user && password == valid_pwd {
+                auth.swap(true, Ordering::Relaxed);
+            };
+            let message = if auth.load(Ordering::SeqCst) {
+                "valid auth\n".to_string()
+            } else {
+                "invalid auth\n".to_string()
+            };
+            sender.send(message);
+            return Response::Ok {};
+        }
+        Request::Watch { key } => {
+            if auth.load(Ordering::SeqCst) {
+                let mut watchers = watchers.map.lock().unwrap();
+                let mut senders: Vec<Sender<String>> = match watchers.get(&key) {
+                    Some(mut watchers_vec) => watchers_vec.clone(),
+                    _ => Vec::new(),
+                };
+                senders.push(sender.clone());
+                watchers.insert(key.clone(), senders.clone());
+            }
+            Response::Ok {}
+        }
         Request::Get { key } => {
-            let mut db = db.map.lock().unwrap();
-            let value = match db.get(&key.to_string()) {
-                Some(value) => value,
-                None => "<Empty>",
-            };
-            sender.send(format_args!("value {}\n", value.to_string()).to_string());
-            Response::Value{ key: key.clone(), value: value.to_string() }
-        },
-        Request::Set { key, value  } => {
-            let mut db = db.map.lock().unwrap();
-            db.insert(key.clone().to_string(), value.clone().to_string());
-            println!("Will watch");
-            match watchers.map.lock().unwrap().get(&key) {
-            Some(senders) => {
-                for sender in senders {
-                    println!("Sinding to another client");
-                    sender.send(format_args!("changed {} {}\n", key.to_string(), value.to_string()).to_string());
+            if auth.load(Ordering::SeqCst) {
+                let mut db = db.map.lock().unwrap();
+                let value = match db.get(&key.to_string()) {
+                    Some(value) => value,
+                    None => "<Empty>",
+                };
+                sender.send(format_args!("value {}\n", value.to_string()).to_string());
+                Response::Value {
+                    key: key.clone(),
+                    value: value.to_string(),
                 }
+            } else {
+                Response::Ok {}
             }
-             _ => {}
+        }
+        Request::Set { key, value } => {
+            if auth.load(Ordering::SeqCst) {
+                let mut db = db.map.lock().unwrap();
+                db.insert(key.clone().to_string(), value.clone().to_string());
+                println!("Will watch");
+                match watchers.map.lock().unwrap().get(&key) {
+                    Some(senders) => {
+                        for sender in senders {
+                            println!("Sinding to another client");
+                            sender.send(
+                                format_args!("changed {} {}\n", key.to_string(), value.to_string())
+                                    .to_string(),
+                            );
+                        }
+                    }
+                    _ => {}
+                }
+                Response::Set {
+                    key: key.clone(),
+                    value: value.to_string(),
+                }
+            } else {
+                Response::Ok {}
             }
-            Response::Set{ key: key.clone(), value: value.to_string() }
         }
     }
 }
 
-fn process_message(receiver: & Receiver<String>, writer: &mut BufWriter<& TcpStream>) {
+fn process_message(receiver: &Receiver<String>, writer: &mut BufWriter<&TcpStream>) {
     match receiver.try_recv() {
         Ok(message) => {
             writer.write_fmt(format_args!("{}", message)).unwrap();
             writer.flush();
-        },
-        _ => thread::sleep(time::Duration::from_millis(2))
+        }
+        _ => thread::sleep(time::Duration::from_millis(2)),
     }
 }
 
-fn handle_client(stream: TcpStream, db: Arc<Database>, watchers:Arc<Watchers>) {
-	let mut reader = BufReader::new(&stream);
-	let  writer = &mut BufWriter::new(&stream);
+fn handle_client(stream: TcpStream, db: Arc<Database>, watchers: Arc<Watchers>) {
+    let mut reader = BufReader::new(&stream);
+    let writer = &mut BufWriter::new(&stream);
     let (sender, receiver): (Sender<String>, Receiver<String>) = channel();
-
-	loop {  
-            let mut buf = String::new();
-            let read_line = reader.read_line(&mut buf);
-            stream.set_nonblocking(true).unwrap();
-            match read_line {
-                Ok(_) => {
-                    process_request(&buf, watchers.clone(), sender.clone(), db.clone());
-                    println!("Command print: {}", buf);
-                },
-                _ => process_message(&receiver, writer)
+    let auth = Arc::new(AtomicBool::new(false));
+    loop {
+        let mut buf = String::new();
+        let read_line = reader.read_line(&mut buf);
+        stream.set_nonblocking(true).unwrap();
+        match read_line {
+            Ok(_) => {
+                process_request(
+                    &buf,
+                    watchers.clone(),
+                    sender.clone(),
+                    db.clone(),
+                    auth.clone(),
+                );
+                println!("Command print: {}", buf);
             }
+            _ => process_message(&receiver, writer),
+        }
     }
 }
-fn start_tcp_client( watchers:Arc<Watchers>, db: Arc<Database>) {
+fn start_tcp_client(watchers: Arc<Watchers>, db: Arc<Database>) {
     match TcpListener::bind("127.0.0.1:9001") {
         Ok(listener) => {
             for stream in listener.incoming() {
-                let db  = db.clone();
+                let db = db.clone();
                 let watchers = watchers.clone();
-                thread::spawn(move || {
-                    match stream {
-                        Ok(socket) => {
-                            handle_client(socket, db, watchers);
-                        },
-                        _ => ()
+                thread::spawn(move || match stream {
+                    Ok(socket) => {
+                        handle_client(socket, db, watchers);
                     }
+                    _ => (),
                 });
             }
-        },
+        }
         _ => {
             println!("Bind error");
         }
     };
 }
 // Server WebSocket handler
-    struct Server {
-        out: ws::Sender,
-        sender:Sender<String>,
-        watchers:Arc<Watchers>, 
-        db: Arc<Database>,
-    }
+struct Server {
+    out: ws::Sender,
+    sender: Sender<String>,
+    watchers: Arc<Watchers>,
+    db: Arc<Database>,
+    auth: Arc<AtomicBool>,
+}
 
-    impl Handler for Server {
-        fn on_open(&mut self, _: ws::Handshake) -> ws::Result<()> {
-            let (sender, receiver): (Sender<String>, Receiver<String>) = channel();
-            self.sender = sender;
-            let sender = self.out.clone();
-            let readTread = thread::spawn(move ||{
-                loop {
-                    match receiver.recv() {
-                        Ok(message) => {
-                            match message.as_ref() {
-                                TO_CLOSE => {
-                                    println!("Closing server connection");
-                                    break;
-                                }
-                                _ => {
-                                    sender.send(message).unwrap();
-                                }
-                            }
-                        },
-                        _ => thread::sleep(time::Duration::from_millis(2))
+impl Handler for Server {
+    fn on_open(&mut self, _: ws::Handshake) -> ws::Result<()> {
+        let (sender, receiver): (Sender<String>, Receiver<String>) = channel();
+        self.sender = sender;
+        let sender = self.out.clone();
+        let readTread = thread::spawn(move || loop {
+            match receiver.recv() {
+                Ok(message) => match message.as_ref() {
+                    TO_CLOSE => {
+                        println!("Closing server connection");
+                        break;
                     }
-                }
-            });
-            Ok(())
-        }
-        fn on_message(&mut self, msg: Message) -> ws::Result<()> {
-            println!("Server got message '{}'. ", msg);
-            let message = msg.as_text().unwrap();
-            process_request(&message, self.watchers.clone(), self.sender.clone(), self.db.clone());
-            println!("Server got message 1 '{}'. ", message);
-            Ok(()) 
-        }
-
-        fn on_close(&mut self, code: CloseCode, reason: &str) {
-            println!("WebSocket closing for ({:?}) {}", code, reason);
-            self.sender.send(TO_CLOSE.to_string()).unwrap();//Closes the read thread
-            //println!("Shutting down server after first connection closes.");
-            //self.out.shutdown().unwrap();
-        }
+                    _ => {
+                        sender.send(message).unwrap();
+                    }
+                },
+                _ => thread::sleep(time::Duration::from_millis(2)),
+            }
+        });
+        Ok(())
     }
 
-fn start_web_socket_client(watchers:Arc<Watchers>, db: Arc<Database>)  {
-   let server = thread::spawn(move ||{ 
-                                let (sender, _): (Sender<String>, Receiver<String>) = channel();
-                                ws::Builder::new()
-                                .with_settings(ws::Settings {
-                                    max_connections: 10000,
-                                    ..ws::Settings::default()
-                                })
-                                .build(move |out| Server { out, db: db.clone(), watchers: watchers.clone(), sender: sender.clone()})
-                                .unwrap()
-                                .listen("0.0.0.0:3012", ).unwrap()
-                            });
+    fn on_message(&mut self, msg: Message) -> ws::Result<()> {
+        println!("Server got message '{}'. ", msg);
+        let message = msg.as_text().unwrap();
+        process_request(
+            &message,
+            self.watchers.clone(),
+            self.sender.clone(),
+            self.db.clone(),
+            self.auth.clone(),
+        );
+        println!("Server got message 1 '{}'. ", message);
+        Ok(())
+    }
+
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        println!("WebSocket closing for ({:?}) {}", code, reason);
+        self.sender.send(TO_CLOSE.to_string()).unwrap(); //Closes the read thread
+    }
+}
+
+fn start_web_socket_client(watchers: Arc<Watchers>, db: Arc<Database>) {
+    let server = thread::spawn(move || {
+        let (sender, _): (Sender<String>, Receiver<String>) = channel();
+        ws::Builder::new()
+            .with_settings(ws::Settings {
+                max_connections: 10000,
+                ..ws::Settings::default()
+            })
+            .build(move |out| Server {
+                out,
+                db: db.clone(),
+                watchers: watchers.clone(),
+                sender: sender.clone(),
+                auth: Arc::new(AtomicBool::new(false)),
+            })
+            .unwrap()
+            .listen("0.0.0.0:3012")
+            .unwrap()
+    });
 
     println!("WebSocket started ");
-   let _ = server.join();
+    let _ = server.join();
 }
 
 fn main() -> Result<(), String> {
@@ -195,11 +253,9 @@ fn main() -> Result<(), String> {
     let watchers = Arc::new(Watchers {
         map: Mutex::new(initial_watchers),
     });
-    let  watchers_socket = watchers.clone();
-    let  db_socket = db.clone();
-    let wsThread = thread::spawn(|| {
-        start_web_socket_client(watchers_socket, db_socket)
-    });
+    let watchers_socket = watchers.clone();
+    let db_socket = db.clone();
+    let wsThread = thread::spawn(|| start_web_socket_client(watchers_socket, db_socket));
     start_tcp_client(watchers.clone(), db.clone());
     Ok(())
 }
