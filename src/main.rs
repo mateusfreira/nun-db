1479ada6dc386f016384edc50bac515a4a0bdd77
Mateus Santos <mateus.freira@gmail.com>
2019-11-24 15:31:32 -0300

    Fix load and snap shot thread issue

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,15 @@ use lib::*;
 fn main() -> Result<(), String> {
     env_logger::init();
     let dbs = lib::db_ops::create_init_dbs();
-    let db_socket = dbs.clone();
+
+    let timer = timer::Timer::new();
     let db_snap = dbs.clone();
+    let _snapshot_thread = thread::spawn(|| lib::disk_ops::start_snap_shot_timer(timer, db_snap));
+
+    let db_socket = dbs.clone();
     // Netwotk threds
     let _ws_thread = thread::spawn(|| lib::ws_ops::start_web_socket_client(db_socket));
     lib::tcp_ops::start_tcp_client(dbs.clone());
     // Disck thread
-    let timer = timer::Timer::new();
-    let _snapshot_thread = thread::spawn(|| lib::disk_ops::start_snap_shot_timer(timer, db_snap));
     Ok(())
 }
--- todo.md
+++ todo.md
@@ -1,6 +1,6 @@
--  [ ] add secret token to create datbase 
--  [ ]  Send and errro if the DB does not exits
- - [ ] Read https://www.microsoft.com/en-us/research/uploads/prod/2018/03/faster-sigmod18.pdf
-
- - [ ] Reduce the size of the docker image
+- [ ] Add un-watch command 
+- [ ] add secret token to create datbase 
+- [ ]  Send and errro if the DB does not exits
+- [ ] Read https://www.microsoft.com/en-us/research/uploads/prod/2018/03/faster-sigmod18.pdf
+- [ ] Reduce the size of the docker image
 
