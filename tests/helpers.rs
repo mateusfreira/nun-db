ccce16d997acaa0163426defec52402a60a34bd1
Mateus Santos <mateus.freira@gmail.com>
2021-11-14 10:16:46 -0300

    Introduces tests to election and replication

--- tests/helpers.rs
+++ tests/helpers.rs
@@ -2,8 +2,10 @@
 pub mod helpers {
 
     use assert_cmd::prelude::*; // Add methods on commands
+    use std::process::Child;
     use std::process::Command;
     use std::{thread, time};
+
     pub const USER_NAME: &'static str = "mateus";
     pub const PWD: &'static str = "mateus";
 
@@ -17,12 +19,12 @@ pub mod helpers {
     pub const SECOUNDARY_WS_ADDRESS: &'static str = "127.0.0.1:3059";
     pub const SECOUNDAR_HTTP_URI: &'static str = "http://127.0.0.1:9093";
 
-    pub const SECOUNDARY2_HTTP_ADDRESS: &'static str = "127.0.0.1:3059";
+    pub const SECOUNDARY2_HTTP_ADDRESS: &'static str = "127.0.0.1:9094";
+    pub const SECOUNDAR2_HTTP_URI: &'static str = "http://127.0.0.1:9094";
     pub const SECOUNDARY2_TCP_ADDRESS: &'static str = "127.0.0.1:3016";
     pub const SECOUNDARY2_WS_ADDRESS: &'static str = "127.0.0.1:3060";
-    pub const SECOUNDAR2_HTTP_URI: &'static str = "http://127.0.0.1:3059";
 
-    pub const REPLICATE_SET_ADDRS: &'static str = "127.0.0.1:3017,127.0.0.1:3018";
+    pub const REPLICATE_SET_ADDRS: &'static str = "127.0.0.1:3017,127.0.0.1:3018,127.0.0.1:3016";
 
     pub fn start_db() -> std::process::Child {
         let time_to_start = time::Duration::from_secs(1);
@@ -67,8 +69,36 @@ pub mod helpers {
             .env("NUN_DBS_DIR", "/tmp/dbs1")
             .spawn()
             .unwrap();
-        wait_secounds(10);
+        wait_secounds(5);
         db_process
+    }
+
+    pub fn start_secoundary_2() -> std::process::Child {
+        let mut cmd = Command::cargo_bin("nun-db").unwrap();
+        let db_process = cmd
+            .args(["-p", USER_NAME])
+            .args(["--user", PWD])
+            .arg("start")
+            .args(["--http-address", SECOUNDARY2_HTTP_ADDRESS])
+            .args(["--tcp-address", SECOUNDARY2_TCP_ADDRESS])
+            .args(["--ws-address", SECOUNDARY2_WS_ADDRESS])
+            .args(["--replicate-address", REPLICATE_SET_ADDRS])
+            .env("NUN_DBS_DIR", "/tmp/dbs2")
+            .spawn()
+            .unwrap();
+        wait_secounds(5);
+        db_process
+    }
+
+    pub fn nundb_exec(host: &String, command: &String) -> assert_cmd::assert::Assert {
+        Command::cargo_bin("nun-db")
+            .unwrap()
+            .args(["-p", "mateus"])
+            .args(["--user", "mateus"])
+            .args(["--host", host])
+            .arg("exec")
+            .arg(command)
+            .assert()
     }
 
     pub fn wait_secounds(time: u64) {
@@ -78,7 +108,23 @@ pub mod helpers {
 
     pub fn clean_env() {
         let mut cmd = Command::new("bash");
-        let clen_cmd = cmd.args(["-c", "rm -Rf /tmp/dbs&&rm -Rf /tmp/dbs1&&rm -Rf /tmp/dbs2/&&mkdir  /tmp/dbs2/&&mkdir  /tmp/dbs1&&mkdir  /tmp/dbs&&killall nun-db|true"]);
+        let clen_cmd = cmd.args(["-c", "rm -Rf /tmp/dbs||true&&rm -Rf /tmp/dbs1||true&&rm -Rf /tmp/dbs2||true/&&mkdir  /tmp/dbs2||true/&&mkdir /tmp/dbs1||true&&mkdir /tmp/dbs||true&&killall nun-db|true"]);
         clen_cmd.assert().success();
+    }
+
+    pub fn start_3_replicas() -> (Child, Child, Child) {
+        (
+        start_primary(),
+        start_secoundary(),
+        start_secoundary_2()
+        )
+    }
+
+
+    pub fn kill_replicas(mut replica_processes: (Child, Child, Child)) -> Result<(), Box<dyn std::error::Error>>{
+        replica_processes.0.kill()?;
+        replica_processes.1.kill()?;
+        replica_processes.2.kill()?;
+        Ok(())
     }
 }
--- tests/replication-tests.rs
+++ tests/replication-tests.rs
@@ -2,59 +2,59 @@
 pub mod helpers;
 mod tests {
     use crate::helpers::*;
-    use assert_cmd::prelude::*; // Add methods on commands
     use predicates::prelude::*; // Used for writing assertions
-    use std::process::Command;
-
-    #[test]
-    fn should_start_the_primary() -> Result<(), Box<dyn std::error::Error>> {
-        let mut db_process = helpers::start_primary();
-        let mut cmd = Command::cargo_bin("nun-db")?;
-        let get_cmd = cmd.args(["-p", "mateus"])
-            .args(["--user", "mateus"])
-            .args(["--host", helpers::PRIMARY_HTTP_URI])
-            .arg("exec")
-            .arg("create-db test test-pwd; use-db test test-pwd;set-safe name 0 mateus; get-safe name");
-
-        get_cmd
-            .assert()
-            .success()
-            .stdout(predicate::str::contains("value-version 1 mateus"));
-        db_process.kill()?;
-        Ok(())
-    }
 
     #[test]
     fn should_replicate_as_expected() -> Result<(), Box<dyn std::error::Error>> {
         helpers::clean_env();
-        let mut primary_process = helpers::start_primary();
-        let mut secoundary_process = helpers::start_secoundary();
-        let mut set_primary = Command::cargo_bin("nun-db")?;
-        let _ = set_primary
-            .args(["-p", "mateus"])
-            .args(["--user", "mateus"])
-            .args(["--host", helpers::PRIMARY_HTTP_URI])
-            .arg("exec")
-            .arg("create-db test test-pwd; use-db test test-pwd;set-safe name 0 mateus;")
-            .assert()
-            .success()
-            .stdout(predicate::str::contains("empty;empty"));
+        let replicas_processes = helpers::start_3_replicas();
+        helpers::nundb_exec(
+            &helpers::PRIMARY_HTTP_URI.to_string(),
+            &String::from("create-db test test-pwd; use-db test test-pwd;set-safe name 0 mateus;"),
+        )
+        .success()
+        .stdout(predicate::str::contains("empty;empty"));
+
         helpers::wait_secounds(1); //Wait 1s to the replication
 
-        let mut get_secoundary = Command::cargo_bin("nun-db")?;
-        let get_cmd = get_secoundary
-            .args(["-p", "mateus"])
-            .args(["--user", "mateus"])
-            .args(["--host", helpers::SECOUNDAR_HTTP_URI])
-            .arg("exec")
-            .arg("use-db test test-pwd;get-safe name");
+        helpers::nundb_exec(
+            &helpers::SECOUNDAR_HTTP_URI.to_string(),
+            &String::from("use-db test test-pwd;get-safe name"),
+        )
+        .success()
+        .stdout(predicate::str::contains("value-version 1 mateus"));
 
-        get_cmd
-            .assert()
-            .success()
-            .stdout(predicate::str::contains("value-version 1 mateus"));
-        primary_process.kill()?;
-        secoundary_process.kill()?;
+        helpers::nundb_exec(
+            &helpers::SECOUNDAR2_HTTP_URI.to_string(),
+            &String::from("use-db test test-pwd;get-safe name"),
+        )
+        .success()
+        .stdout(predicate::str::contains("value-version 1 mateus"));
+
+        //Save from secoundary 2 read from primary and secoundary 1
+        helpers::nundb_exec(
+            &helpers::SECOUNDAR2_HTTP_URI.to_string(),
+            &String::from("use-db test test-pwd;set-safe name 1 maria;"),
+        )
+        .success()
+        .stdout(predicate::str::contains("empty;empty"));
+
+        helpers::nundb_exec(
+            &helpers::PRIMARY_HTTP_URI.to_string(),
+            &String::from("use-db test test-pwd;get name;get-safe name"),
+        )
+        .success()
+        .stdout(predicate::str::contains("value maria"))
+        .stdout(predicate::str::contains("value-version 2 maria"));
+
+        helpers::nundb_exec(
+            &helpers::SECOUNDAR_HTTP_URI.to_string(),
+            &String::from("use-db test test-pwd;get name;get-safe name"),
+        )
+        .success()
+        .stdout(predicate::str::contains("value maria"))
+        .stdout(predicate::str::contains("value-version 2 maria"));
+        helpers::kill_replicas(replicas_processes)?;
         Ok(())
     }
 }
