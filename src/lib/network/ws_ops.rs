f1c18cd4cee6c5ada1e3e48a34e356a69d786bf5
Mateus Freira <mateus.freira@gmail.com>
2024-04-20 15:19:22 -0300

    clean todo

--- src/lib/network/ws_ops.rs
+++ src/lib/network/ws_ops.rs
@@ -66,7 +66,6 @@ impl Handler for Server {
         );
         let messages_part = message.split(";");
         messages_part.for_each(|message| {
-            // @todo unit tests it
             match process_request(&message, &self.dbs, &mut self.client) {
                 Response::Error { msg } => {
                     log::debug!("Error: {}", msg);
