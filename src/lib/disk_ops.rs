0cd668aec2180ffa5607015b92ad7fb021c6ac4b
Mateus Freira <mateus.freira@gmail.com>
2024-05-15 20:12:31 -0300

    skip slow tests

--- src/lib/disk_ops.rs
+++ src/lib/disk_ops.rs
@@ -1375,7 +1375,7 @@ mod tests {
     }
 
     #[test]
-    #[cfg(not(tarpaulin_include))]
+    #[cfg(not(tarpaulin))]
     fn restore_should_be_fast() {
         let (dbs, db_name, db) = create_db_with_10k_keys();
         clean_all_db_files(&db_name);
@@ -1422,7 +1422,7 @@ mod tests {
     }
 
     #[test]
-    #[cfg(not(tarpaulin_include))]
+    #[cfg(not(tarpaulin))]
     fn should_store_all_keys_if_reclame_space_mode() {
         let (dbs, db_name, db) = create_db_with_10k_keys();
         clean_all_db_files(&db_name);
