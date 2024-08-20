bf4d60b0f3b4cc96db62b4940d4e58f16cf76a95
Mateus Freira <mateus.freira@gmail.com>
2024-08-19 21:13:40 -0300

    improve set safe tests

--- tests/set-safe-tests.rs
+++ tests/set-safe-tests.rs
@@ -7,9 +7,10 @@ mod tests {
     #[test]
     fn should_safe_sate_a_value() -> Result<(), Box<dyn std::error::Error>> {
         helpers::clean_env();
-        let mut db_process = helpers::start_primary();
-        helpers::nundb_exec_primary(
-            "create-db test test-pwd; use-db test test-pwd;set-safe name 0 mateus; get-safe name",
+        let (mut db_process, primary_uri )= helpers::start_primary_uri(3030);
+        helpers::nundb_exec(
+            &primary_uri,
+            "create-db test-1 test-pwd; use-db test-1 test-pwd;set-safe name 0 mateus; get-safe name",
         )
         .stdout(predicate::str::contains("value-version 1 mateus"));
         db_process.kill()?;
@@ -19,14 +20,14 @@ mod tests {
     #[test]
     fn should_not_set_if_set_safe_is_invalid() -> Result<(), Box<dyn std::error::Error>> {
         helpers::clean_env();
-        let mut db_process = helpers::start_primary();
-        helpers::nundb_exec_primary("create-db test test-pwd none; use-db test test-pwd;set-safe name 0 mateus; set-safe name 0 maria;get-safe name")
+        let (mut db_process, primary_uri )= helpers::start_primary_uri(1041);
+        helpers::nundb_exec(&primary_uri, "create-db test-2 test-pwd none; use-db test-2 test-pwd;set-safe name 0 mateus; set-safe name 0 maria;get-safe name")
             .stdout(predicate::str::contains("value-version 1 mateus"));
 
-        helpers::nundb_exec_primary("use-db test test-pwd;set-safe name 0 amanda;")
+        helpers::nundb_exec(&primary_uri,"use-db test-2 test-pwd;set-safe name 0 amanda;")
             .stdout(predicate::str::contains("Invalid version!"));
 
-        helpers::nundb_exec_primary("use-db test test-pwd;set-safe name 1 amanda;")
+        helpers::nundb_exec(&primary_uri, "use-db test-2 test-pwd;set-safe name 1 amanda;")
             .stdout(predicate::str::contains("empty;empty"));
         db_process.kill()?;
         Ok(())
@@ -35,8 +36,8 @@ mod tests {
     #[test]
     fn should_set_if_set_safe_is_valid() -> Result<(), Box<dyn std::error::Error>> {
         helpers::clean_env();
-        let mut db_process = helpers::start_primary();
-        helpers::nundb_exec_primary("create-db test test-pwd; use-db test test-pwd;set-safe name 0 mateus; set-safe name 2 maria;get-safe name")
+        let (mut db_process, primary_uri)= helpers::start_primary_uri(2040);
+        helpers::nundb_exec(&primary_uri,"create-db test-3 test-pwd; use-db test-3 test-pwd;set-safe name 0 mateus; set-safe name 2 maria;get-safe name")
             .stdout(predicate::str::contains("value-version 3 maria"));
 
         db_process.kill()?;
