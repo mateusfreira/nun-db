ac3f73b9783e0d1c1b88213e00742f4d884c28c5
Mateus Freira <mateus.freira@gmail.com>
2024-12-01 06:51:57 -0300

    Basic working

--- src/lib/storage/s3.rs
+++ src/lib/storage/s3.rs
@@ -1,8 +1,9 @@
-use std::io::Write;
+use std::{future::Future, io::Write};
 
 use aws_config::Region;
 use aws_sdk_s3::config::Credentials;
 use bytes::{BufMut, BytesMut};
+use tokio::runtime::Runtime;
 
 use crate::bo::{Database, Databases, Value, ValueStatus};
 
@@ -36,84 +37,86 @@ fn get_keys_to_push(db: &Database, reclame_space: bool) -> Vec<(String, Value)> 
 pub struct S3Storage {}
 impl S3Storage {
     pub fn storage_data_disk(db: &Database, reclame_space: bool, db_name: &String) -> u32 {
+        let rt  = Runtime::new().unwrap();
         let keys_to_update = get_keys_to_push(db, reclame_space);
 
         let key_buffer = BytesMut::with_capacity(OP_RECORD_SIZE * 10);
         //@todo should this really be te buffer size of the values????
         let value_buffer = BytesMut::with_capacity(OP_RECORD_SIZE * 10);
+        {
+            let mut keys_file = key_buffer;//.writer();
+            let mut values_file = value_buffer.writer();
+            let current_key_file_size = 0;
 
-        let mut keys_file = key_buffer.writer();
-        let mut values_file = value_buffer.writer();
-        let current_key_file_size = 0;
+            //let (mut values_file, current_value_file_size) = get_values_file_append_mode(&db_name, reclame_space);
+            // To inplace update
+            log::debug!("current_key_file_size: {}", current_key_file_size);
 
-        //let (mut values_file, current_value_file_size) = get_values_file_append_mode(&db_name, reclame_space);
-        // To inplace update
-        log::debug!("current_key_file_size: {}", current_key_file_size);
+            let mut value_addr = u64::from(0 as u64); //current_value_file_size;
+            let mut next_key_addr = current_key_file_size;
+            let mut changed_keys = 0;
 
-        let mut value_addr = u64::from(0 as u64); //current_value_file_size;
-        let mut next_key_addr = current_key_file_size;
-        let mut changed_keys = 0;
+            for (key, value) in keys_to_update {
+                match value.state {
+                    ValueStatus::New => {
+                        if !reclame_space {
+                            panic!("Values Ok should never get here")
+                        } else {
+                            changed_keys = changed_keys + 1;
 
-        for (key, value) in keys_to_update {
-            match value.state {
-                ValueStatus::Ok => {
-                    if !reclame_space {
-                        panic!("Values Ok should never get here")
-                    } else {
-                        changed_keys = changed_keys + 1;
+                            values_file.write(&value.value.len().to_le_bytes()).unwrap();
+                            //8bytes
+                            let value_as_bytes = value.value.as_bytes();
+                            //Nth bytes
+                            values_file.write(&value_as_bytes).unwrap();
+                            //4 bytes
+                            values_file.write(&value.state.to_le_bytes()).unwrap();
+                            let record_size = (U64_SIZE + value_as_bytes.len() + VERSION_SIZE) as u64;
+                            log::debug!(
+                                "Write key: {}, addr: {} value_addr: {} ",
+                                key,
+                                next_key_addr,
+                                value_addr
+                            );
+                            // Append key file
+                            // Write key
 
-                        values_file.write(&value.value.len().to_le_bytes()).unwrap();
-                        //8bytes
-                        let value_as_bytes = value.value.as_bytes();
-                        //Nth bytes
-                        values_file.write(&value_as_bytes).unwrap();
-                        //4 bytes
-                        values_file.write(&value.state.to_le_bytes()).unwrap();
-                        let record_size = (U64_SIZE + value_as_bytes.len() + VERSION_SIZE) as u64;
-                        log::debug!(
-                            "Write key: {}, addr: {} value_addr: {} ",
-                            key,
-                            next_key_addr,
-                            value_addr
-                        );
-                        // Append key file
-                        // Write key
-
-                        let len = key.len();
-                        //8bytes
-                        keys_file.write(&len.to_le_bytes()).unwrap();
-                        //Nth bytes
-                        keys_file.write(&key.as_bytes()).unwrap();
-                        //4 bytes
-                        keys_file.write(&value.version.to_le_bytes()).unwrap();
-                        //8 bytes
-                        keys_file.write(&value_addr.to_le_bytes()).unwrap();
-                        let key_size = get_key_disk_size(key.len());
-                        db.set_value_as_ok(
-                            &key,
-                            &value,
-                            value_addr,
-                            next_key_addr,
-                            Databases::next_op_log_id(),
-                        );
-                        value_addr = value_addr + record_size;
-                        next_key_addr = next_key_addr + key_size;
+                            let len = key.len();
+                            //8bytes
+                            keys_file.put_slice(&len.to_le_bytes());
+                            //Nth bytes
+                            keys_file.put_slice(&key.as_bytes());
+                            //4 bytes
+                            keys_file.put_slice(&value.version.to_le_bytes());
+                            //8 bytes
+                            keys_file.put_slice(&value_addr.to_le_bytes());
+                            let key_size = get_key_disk_size(key.len());
+                            db.set_value_as_ok(
+                                &key,
+                                &value,
+                                value_addr,
+                                next_key_addr,
+                                Databases::next_op_log_id(),
+                            );
+                            value_addr = value_addr + record_size;
+                            next_key_addr = next_key_addr + key_size;
+                        }
                     }
+                    ValueStatus::Ok => {},
+                    ValueStatus::Updated => {}
+                    ValueStatus::Deleted => {}
                 }
-                ValueStatus::New => {}
-
-                ValueStatus::Updated => {}
-                ValueStatus::Deleted => {}
             }
-        }
 
-        keys_file.flush().unwrap();
+            //keys_file.flush().unwrap();
+            rt.block_on(S3Storage::store_buffer_to_s3(keys_file));
+        }
         //keys_file.
-        S3Storage::store_buffer_to_s3(key_buffer);
-        values_file.flush().unwrap();
+        //values_file.flush().unwrap();
         //write_metadata_file(db_name, db);
-        log::debug!("snapshoted {} keys", changed_keys);
-        changed_keys
+        //log::debug!("snapshoted {} keys", changed_keys);
+        //changed_keys
+        0
     }
 
     async fn store_buffer_to_s3(mut buff: BytesMut) -> Option<bool> {
